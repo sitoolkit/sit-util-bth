286060a3f7281fbfcb9a4841cd8585c8eff8476c
MasaoBlue <16271994+MasaoBlue@users.noreply.github.com>
2018-12-12 13:00:05 +0900

    Read proxy setting from sitoolkit config.properties (#2)

--- src/main/java/io/sitoolkit/util/buildtoolhelper/proxysetting/ProxySetting.java
+++ src/main/java/io/sitoolkit/util/buildtoolhelper/proxysetting/ProxySetting.java
@@ -4,6 +4,9 @@ import java.util.HashMap;
 
 import org.apache.commons.lang3.StringUtils;
 
+import lombok.Data;
+
+@Data
 public class ProxySetting {
     private String proxyActive = "false";
 
@@ -11,48 +14,23 @@ public class ProxySetting {
 
     private String proxyPort = "";
 
+    private String proxyUser = "";
+
+    private String proxyPassword = "";
+
     private String nonProxyHosts = "";
 
     public void setRegistryResult(HashMap<String, String> proxy) {
-        setProxySettings(proxy.get("host"), proxy.get("port"), proxy.get("nonProxyHosts"));
+        setProxySettings(proxy.get("host"), proxy.get("port"), "", "", proxy.get("nonProxyHosts"));
     }
 
-    public void setProxySettings(String host, String port, String nonProxyHosts) {
+    public void setProxySettings(String host, String port, String user, String password,
+            String nonProxyHosts) {
         this.proxyActive = "true";
         this.proxyHost = host;
         this.proxyPort = port;
-        this.nonProxyHosts = nonProxyHosts;
-    }
-
-    public String getProxyActive() {
-        return proxyActive;
-    }
-
-    public void setProxyActive(String proxyActive) {
-        this.proxyActive = proxyActive;
-    }
-
-    public String getProxyHost() {
-        return proxyHost;
-    }
-
-    public void setProxyHost(String proxyHost) {
-        this.proxyHost = proxyHost;
-    }
-
-    public String getProxyPort() {
-        return proxyPort;
-    }
-
-    public void setProxyPort(String proxyPort) {
-        this.proxyPort = proxyPort;
-    }
-
-    public String getNonProxyHosts() {
-        return nonProxyHosts;
-    }
-
-    public void setNonProxyHosts(String nonProxyHosts) {
+        this.proxyUser = user;
+        this.proxyPassword = password;
         this.nonProxyHosts = nonProxyHosts;
     }
 
