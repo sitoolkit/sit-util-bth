c1d8b7edc2d795cfa9b0e9e8e98b94bafacaac52
kakeru.k <kakl2612@gmail.com>
2019-10-17 18:26:58 +0900

    外部プロセス実行中にログ出力が停止してしまう事象の修正。

--- src/main/java/io/sitoolkit/util/buildtoolhelper/process/ProcessExecutor.java
+++ src/main/java/io/sitoolkit/util/buildtoolhelper/process/ProcessExecutor.java
@@ -8,7 +8,9 @@ import java.io.UncheckedIOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import lombok.extern.slf4j.Slf4j;
 
